e46f1c74fa071aef7415e5c96c78fc7579493785
Benjamin Rappo <brappo@z1r4p4.42lyon.fr>
2024-12-13 11:58:36 +0100

    added a function in the main-tests.cpp to print KO or OK if a bool is true or false

--- tests/scripts/main-test.cpp
+++ tests/scripts/main-test.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
+
+std::string	test(int value)
+{
+	if (value == 0)
+		return ("test");
+	return ("truc");
+}
+
+void	verify(bool test)
+{
+	if (test)
+		printf("\033[0;32mOK\033[0m\n");
+	else
+		printf("\033[0;31mKO\033[0m\n");
+}
 
 int	main(void)
 {
-	std::cout << "Test" << '\n';
+	verify(test(0) == "test");
+	verify(test(1) == "test");
+	verify(test(1) == "truc");
 }
