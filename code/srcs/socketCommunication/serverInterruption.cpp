c3f491b2965cbda28bea7a3c9099e547e6e820e6
Benjamin RAPPO <benjamin.rappo69@gmail.com>
2025-01-07 10:39:32 +0100

    added comments to the serverInterruption file

--- code/srcs/socketCommunication/serverInterruption.cpp
+++ code/srcs/socketCommunication/serverInterruption.cpp
@@ -2,13 +2,23 @@
 
 #include "socketCommunication.hpp"
 
+/**
+ * @brief A signal/thread safe variable that store the received signal.
+ * If no signal has been received, its value is NO_SIGNAL
+ */
 volatile std::sig_atomic_t	signalStatus = NO_SIGNAL;
 
+/**
+ * @brief Set the signalStatus value
+ */
 void	signalHandler(int signal)
 {
 	signalStatus = signal;
 }
 
+/**
+ * @brief If a signal ha sbeen received, return it, otherwise, return NO_SIGNAL
+ */
 int	getSignalStatus()
 {
 	return (signalStatus);
