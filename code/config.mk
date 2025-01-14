6d67d182f6e5525acd239f60fc028d6fc7c80896
sben-rho <sben-rho@42lyon.fr>
2025-01-14 14:45:38 +0100

    Add requestManagement to the config

--- code/config.mk
+++ code/config.mk
@@ -5,4 +5,9 @@ SOCKET					:=	$(addprefix $(SOCKET_DIR), $(SOCKET_FILES))
 CONFIGURATION_DIR		:=	configuration/
 CONFIGURATION_FILES		:=	Configuration.cpp	\
 							ServerConfiguration.cpp
-CONFIGURATION			:=	$(addprefix $(SOCKET_DIR), $(SOCKET_FILES))
+CONFIGURATION			:=	$(addprefix $(CONFIGURATION_DIR), $(CONFIGURATION_FILES))
+
+REQUEST_DIR				:=	requestManagement/
+REQUEST_FILES			:=	GetRequestUtils.cpp	\
+							GetRequest.cpp
+REQUEST					:=	$(addprefix $(REQUEST_DIR), $(REQUEST_FILES))
