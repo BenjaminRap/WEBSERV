cc13bc14d7a0f00c825bca3f23fb91b203400716
Benjamin RAPPO <benjamin.rappo69@gmail.com>
2025-01-21 11:31:05 +0100

    added comments to the RawResponse constructor

--- code/srcs/dataFlow/RawResponse.cpp
+++ code/srcs/dataFlow/RawResponse.cpp
@@ -5,6 +5,14 @@
 
 #include "RawResponse.hpp"
 
+/**
+ * @brief Create a RawResponse instance.
+ * @param buffer The buffer used to store the firstPart.
+ * @param bufferCapacity The maximum number of chars that the buffer can store.
+ * @param bodyFd The fd of the body.
+ * @param bodyBuffer The FlowBuffer used to redirect the data from the body to
+ * the client socket.
+ */
 RawResponse::RawResponse
 (
 	char *buffer,
