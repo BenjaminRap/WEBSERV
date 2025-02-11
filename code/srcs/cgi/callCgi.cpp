9059371e7557bce7df8693f5aa6b7f527a488339
brappo <brappo@PCTOPACHAT.>
2025-02-11 11:41:55 +0100

    Now added a pipe for the program stdin and stdout

--- code/srcs/cgi/callCgi.cpp
+++ code/srcs/cgi/callCgi.cpp
@@ -1,13 +1,40 @@
 #include <unistd.h>
+#include <cstdlib>
+
+static void	closeTube(int tube[2])
+{
+	close(tube[0]);
+	close(tube[1]);
+}
 
 pid_t	executeCGI(const char *path, char *const *argv, char *const *envp)
 {
 	const pid_t pid = fork();
+	int			tubeSTDIN[2];
+	int			tubeSTDOUT[2];
 	
+	if (pipe(tubeSTDIN) == -1)
+		return (-1);
+	if (pipe(tubeSTDOUT) == -1)
+	{
+		closeTube(tubeSTDIN);
+		return (-1);
+	}
 	if (pid == 0)
 	{
-		if (execve(path, argv, envp) == -1)
-			return (-1);
+		if (dup2(STDIN_FILENO, tubeSTDIN[0]) == -1
+			|| dup2(STDOUT_FILENO, tubeSTDOUT[1]) == -1)
+		{
+			closeTube(tubeSTDIN);
+			closeTube(tubeSTDOUT);
+			std::exit(EXIT_FAILURE);
+		}
+		closeTube(tubeSTDIN);
+		closeTube(tubeSTDOUT);
+		execve(path, argv, envp);
+		std::exit(EXIT_FAILURE);
 	}
+	closeTube(tubeSTDIN);
+	closeTube(tubeSTDOUT);
 	return (pid);	
 }
