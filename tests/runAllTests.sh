a8a679b6e44ccb3d9337b99aa439164066e0dec8
brappo <brappo@PCTOPACHAT.>
2025-03-28 12:02:55 +0100

    Managed to run the requestsTests from the bash script but I messed up the parsing with the getLine change

--- tests/runAllTests.sh
+++ tests/runAllTests.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
 
-runTest()
+runTestChangingMain()
 {
 	echo test: $1
-	(cd ../code && make -s re MAIN=../tests/scripts/$1 && valgrind --leak-check=full --show-leak-kinds=all ./WebServ ../code/configurations/fakenginx.conf)
+	cd ../code
+	make fclean
+	make -s -j16 MAIN=../tests/scripts/$1
+	valgrind --leak-check=full --show-leak-kinds=all ./WebServ ./configurations/fakenginx.conf
+	cd ../tests
 }
 
-runTest flowBufferTests.cpp
-runTest getLineTests.cpp
-runTest aFdDataTests.cpp
-runTest socketsHandlerTests.cpp
+runIndependentTestFromFolder()
+{
+	echo test: $1
+	(cd $2 && c++ $1 -o test && ./test && rm test)
+}
+
+runWebServInBackground()
+{
+	echo running Webserv
+	cd ../code
+	make fclean
+	make -s -j16
+	valgrind --leak-check=full --show-leak-kinds=all ./WebServ ./configurations/fakenginx.conf &> /dev/null &
+	cd ../tests
+}
+
+runNginxInBackground()
+{
+	echo running nginx
+	make all &> /dev/null &
+}
+
+# runTestChangingMain flowBufferTests.cpp
+# runTestChangingMain getLineTests.cpp
+# runTestChangingMain aFdDataTests.cpp
+# runTestChangingMain socketsHandlerTests.cpp
+runWebServInBackground
+PID=$!
+trap "kill -2 $PID 2>/dev/null" EXIT
+runNginxInBackground
+PID=$!
+trap "kill -2 $PID 2>/dev/null" EXIT
+runIndependentTestFromFolder getTests.cpp scripts/requestsTests
+runIndependentTestFromFolder putTests.cpp scripts/requestsTests
+runIndependentTestFromFolder deleteTests.cpp scripts/requestsTests
+docker container ls | awk '{print $1}' | tail -n +2 | xargs docker container stop
