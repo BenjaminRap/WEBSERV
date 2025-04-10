2730d55b58b9212360f7544e8cfb43c5fda2c730
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-04-10 10:10:47 +0200

    Updated the makeawRequest script
    It now parsed the return chunk by chunk and stop when the response is
    finished. We doesn't have to wait for the server to close the
    connection.

--- tests/scripts/requestsTests/makeRawRequest.mjs
+++ tests/scripts/requestsTests/makeRawRequest.mjs
@@ -1,40 +1,77 @@
 import { HTTPParser } from "/usr/local/lib/node_modules/http-parser-js/http-parser.js";
 import * as net from "net";
 
-export function	makeRawRequest(host, port, requestData)
+
+let headers;
+let body;
+let	complete;
+let statusCode;
+let	statusText;
+
+function	reset()
 {
-	return (sendRawRequest(host, port, requestData)
-		.then((response) => parseHttpResponse(response)));
+	headers = {};
+	body = "";
+	complete = false;
+	statusCode = 0;
+	statusText = "";
 }
 
-function sendRawRequest(host, port, requestData)
+function	createResponse()
+{
+	return (new Response(body, {
+		status: statusCode,
+		statusText: statusText,
+		headers: new Headers(headers),
+	}));
+}
+
+export function	makeRawRequest(host, port, requestData)
+{
+	reset();
+	const httpParser = getHTTPParser();
+	return (sendRawRequest(host, port, requestData, httpParser));
+}
+
+function sendRawRequest(host, port, requestData, httpParser)
 {
     return new Promise((resolve, reject) => {
         const client = new net.Socket();
-        let response = '';
 
         client.connect(port, host, () => {
             client.write(requestData);
         });
 
         client.on('data', (data) => {
-            response += data.toString();
+            const chunk = data.toString();
+			const buffer = Buffer.from(chunk, 'utf-8');
+			httpParser.execute(buffer, 0, buffer.length);
+			if (complete == true)
+				client.end();
         });
 
-        client.on('end', () => resolve(response));
+        client.on('end', () => {
+			httpParser.finish();
+			if (complete == true)
+			{
+				console.log("completed !!");
+				return (resolve(createResponse()));
+			}
+			return (reject("Could not finished"));
+		});
 
-        client.on('error', (err) => reject(err));
+        client.on('error', (err) => {
+			console.log("err !!");
+			httpParser.finish();
+			httpParser.finish();
+			return (reject(err));
+		});
     });
 }
 
-function parseHttpResponse(rawText)
+function getHTTPParser()
 {
     const parser = new HTTPParser(HTTPParser.RESPONSE);
-    let headers = {};
-    let body = '';
-	let	complete = false;
-	let statusCode = "";
-	let	statusText = "";
 
     // Callback pour récupérer les headers et les convertir d'un array a une map
     parser[HTTPParser.kOnHeadersComplete] = (response) => {
@@ -43,29 +80,76 @@ function parseHttpResponse(rawText)
 				map[key] = array[index + 1];
             return (map);
         }, {});
-		complete = true;
 		statusCode = response.statusCode;
 		statusText = response.statusMessage;
     };
+
+	parser[HTTPParser.kOnMessageComplete] = () => {
+		complete = true;
+	}
 
     // Callback pour récupérer le corps
     parser[HTTPParser.kOnBody] = (chunk, start, len) => {
         body += chunk.toString('utf-8', start, start + len);
     };
 
-    // Parser la réponse brute
-    const buffer = Buffer.from(rawText, 'utf-8');
-    parser.execute(buffer, 0, buffer.length);
-	parser.finish();
-
-	if (complete === false)
-		throw new Error("Could not parse response !");
-
-    // Créer l'objet Response de Fetch API
-    return (new Response(body, {
-		status: statusCode,
-        statusText: statusText,
-        headers: new Headers(headers),
-    }));
+	return (parser);
 }
 
+// function sendRawRequest(host, port, requestData)
+// {
+//     return new Promise((resolve, reject) => {
+//         const client = new net.Socket();
+//         let response = '';
+//
+//         client.connect(port, host, () => {
+//             client.write(requestData);
+//         });
+//
+//         client.on('data', (data) => {
+//             response += data.toString();
+//         });
+//
+//         client.on('end', () => resolve(response));
+//
+//         client.on('error', (err) => reject(err));
+//     });
+// }
+//
+// function getHTTPParser(rawText)
+// {
+//     const parser = new HTTPParser(HTTPParser.RESPONSE);
+//
+//     // Callback pour récupérer les headers et les convertir d'un array a une map
+//     parser[HTTPParser.kOnHeadersComplete] = (response) => {
+//         headers = response.headers.reduce((map, key, index, array) => {
+//             if (index % 2 === 0)
+// 				map[key] = array[index + 1];
+//             return (map);
+//         }, {});
+// 		complete = true;
+// 		statusCode = response.statusCode;
+// 		statusText = response.statusMessage;
+//     };
+//
+//     // Callback pour récupérer le corps
+//     parser[HTTPParser.kOnBody] = (chunk, start, len) => {
+//         body += chunk.toString('utf-8', start, start + len);
+//     };
+//
+//     // Parser la réponse brute
+//     const buffer = Buffer.from(rawText, 'utf-8');
+//     parser.execute(buffer, 0, buffer.length);
+// 	parser.finish();
+//
+// 	if (complete === false)
+// 		throw new Error("Could not parse response !");
+//
+//     // Créer l'objet Response de Fetch API
+//     return (new Response(body, {
+// 		status: statusCode,
+//         statusText: statusText,
+//         headers: new Headers(headers),
+//     }));
+// }
+//
