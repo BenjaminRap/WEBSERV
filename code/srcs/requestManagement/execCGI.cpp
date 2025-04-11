d1d33c5039c4d8cc40c3c704a2ee953c3dae420c
Simon Abitbol <sabitbol@z2r10p6.42lyon.fr>
2025-04-11 15:32:12 +0200

    execCGI

--- code/srcs/requestManagement/execCGI.cpp
+++ code/srcs/requestManagement/execCGI.cpp
@@ -2,17 +2,54 @@
 #include <csignal>                  // for signal, SIG_ERR, SIGINT, SIGPIPE
 #include <cstdlib>                  // for EXIT_FAILURE
 #include "socketCommunication.hpp"	// for checkError
+#include <sys/wait.h>				// for waitpid()
 
-int	execCGI(char **argv, char **env)
+int	close_on_err(int fd1, int fd2)
 {
+	close(fd1);
+	close(fd2);
+	return (EXIT_FAILURE);
+}
+
+int	execCGI(const char *path, char **argv, char **env)
+{
+
+	//creating the pipe
+	int		fd[2];
+	if (checkError(pipe(fd), -1, "pipe() :"))
+		return (EXIT_FAILURE);
+
+	//fork process
 	pid_t	pid = fork();
+	if (checkError(pid, -1, "fork() :"))
+		return (close_on_err(fd[0], fd[1]));
 
 	if (pid == 0)
 	{
+		//set signals to default for the child process
 		if (checkError(std::signal(SIGINT, SIG_DFL), SIG_ERR, "signal() : "))
-			return (EXIT_FAILURE);
+			return (close_on_err(fd[0], fd[1]));
 		if (checkError(std::signal(SIGPIPE, SIG_DFL), SIG_ERR, "signal() : "))
-			return (EXIT_FAILURE);
-		
+			return (close_on_err(fd[0], fd[1]));
+
+		//redirecting cgi result to the pipe
+		if (checkError(dup2(fd[1], STDOUT_FILENO), -1, "dup2() :"))
+			return (close_on_err(fd[0], fd[1]));
+		close(fd[1]);
+
+		//check acces and execute cgi
+		if (checkError(access(path, X_OK), -1, "access() :"))
+			return (close_on_err(fd[0], STDOUT_FILENO));
+		if (checkError(execve(path, argv, env), -1, "execve() :"))
+			return (close_on_err(fd[0], STDOUT_FILENO));
+	}
+	else
+	{
+		//waiting for CGI to finish
+		int	status;
+		if (checkError(waitpid(pid, &status, 0), -1, "waitpid() :"))
+			return (close_on_err(fd[0], fd[1]));
+		if (status)
+			return (close_on_err(fd[0], fd[1]));
 	}
 }
