d5094e56b2f606ed3a0d2fa10424053e1ba8e6b9
brappo <brappo@PCTOPACHAT.>
2025-05-12 09:47:52 +0200

    Removed try and catch in main because theey are dumb

--- code/srcs/main.cpp
+++ code/srcs/main.cpp
@@ -1,8 +1,6 @@
 #include <csignal>                  // for signal, SIG_ERR, SIGINT, SIGPIPE
 #include <cstdlib>                  // for EXIT_FAILURE
-#include <exception>                // for exception
 #include <iostream>                 // for basic_ostream, operator<<, cerr
-#include <string>                   // for char_traits, basic_string, string
 
 #include "Configuration.hpp"        // for Configuration, DEFAULT_CONFIG_PATH
 #include "parsing.hpp"              // for ft_readfile, parse_file
@@ -18,31 +16,17 @@ int	main(int argc, char **argv)
 		std::cerr << "Too much arguments supplied: webserv (configuration/path)?" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	try
-	{
-		if (checkError(std::signal(SIGINT, signalHandler), SIG_ERR, "signal() : "))
-			return (EXIT_FAILURE);
-		if (checkError(std::signal(SIGTERM, signalHandler), SIG_ERR, "signal() : "))
-			return (EXIT_FAILURE);
-		if (checkError(std::signal(SIGPIPE, SIG_IGN), SIG_ERR, "signal() : "))
-			return (EXIT_FAILURE);
+	if (checkError(std::signal(SIGINT, signalHandler), SIG_ERR, "signal() : "))
+		return (EXIT_FAILURE);
+	if (checkError(std::signal(SIGTERM, signalHandler), SIG_ERR, "signal() : "))
+		return (EXIT_FAILURE);
+	if (checkError(std::signal(SIGPIPE, SIG_IGN), SIG_ERR, "signal() : "))
+		return (EXIT_FAILURE);
 
-		Configuration	conf((argc == 2) ? argv[1] : DEFAULT_CONFIG_PATH);
-		while(getSignalStatus() == NO_SIGNAL)
-		{
-			try
-			{
-				handleIOEvents(conf);
-			}
-			catch(const std::exception& e)
-			{
-				std::cerr << e.what() << '\n';
-			}
-		}
-		return (getReturnCodeWithSignal());
-	}
-	catch(const std::exception& e)
+	Configuration	conf((argc == 2) ? argv[1] : DEFAULT_CONFIG_PATH);
+	while(getSignalStatus() == NO_SIGNAL)
 	{
-		std::cerr << e.what() << '\n';
+		handleIOEvents(conf);
 	}
+	return (getReturnCodeWithSignal());
 }
