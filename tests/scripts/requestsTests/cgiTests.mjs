602d16e95de48e93454d3098a64bd0af0b0405e8
brappo <brappo@PCTOPACHAT.>
2025-05-21 11:18:17 +0200

    Added tests to check if the cgi receive the body properly

--- tests/scripts/requestsTests/cgiTests.mjs
+++ tests/scripts/requestsTests/cgiTests.mjs
@@ -1,6 +1,6 @@
 import { verifyServersAreRunning, exec, printHeader } from "./utils.mjs"
-import { compareGoodRequestWithValues } from "./compareRequests.mjs";
-import { COLOR_GREEN, COLOR_RED, COLOR_RESET } from "./utils.mjs";
+import { compareGoodRequestWithValues, compareBadRequestWithValues } from "./compareRequests.mjs";
+import { COLOR_GREEN, COLOR_RED, COLOR_RESET, createChunkedRequest } from "./utils.mjs";
 
 let	printOK = false;
 
@@ -14,52 +14,71 @@ async function	runGoodGgiTest(target, method, body, headers, statusCode, statusT
 		console.log(COLOR_RED + "[KO] " + COLOR_RESET);
 }
 
+async function	sendCgiChunkedRequest(target, headers, chunks, trailers)
+{
+	const	message = createChunkedRequest(target, headers, chunks, trailers);
+	const	result = await compareBadRequestWithValues(message, 200, "OK", printOK);
+
+	if (result == true)
+		console.log(COLOR_GREEN + "[OK] " + COLOR_RESET);
+	else
+		console.log(COLOR_RED + "[KO] " + COLOR_RESET);
+}
+
 async function runTests()
 {
-	printHeader("Classic Case");
-	await runGoodGgiTest("/cgi/cgiTestClassic.cgi", "GET", null, {}, 200, "OK");
-	printHeader("Chunked Body Output");
-	await runGoodGgiTest("/cgi/cgiBodyChunked.cgi", "GET", null, {}, 200, "OK");
-	printHeader("Cgi Body Too Short");
-	await runGoodGgiTest("/cgi/cgiBodyTooShort.cgi", "GET", null, {}, 200, "OK");
-	printHeader("Cgi Error In Headers");
-	await runGoodGgiTest("/cgi/cgiErrorInHeaders.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi No Content Length");
-	await runGoodGgiTest("/cgi/cgiNoContentLength.cgi", "GET", null, {}, 200, "OK");
-	printHeader("Cgi No Content Type");
-	await runGoodGgiTest("/cgi/cgiNoContentType.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi No Response");
-	await runGoodGgiTest("/cgi/cgiNoResponse.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi No Status Header");
-	await runGoodGgiTest("/cgi/cgiNoStatusHeader.cgi", "GET", null, {}, 200, "OK");
-	printHeader("Cgi No Status Header But Location");
-	await runGoodGgiTest("/cgi/cgiNoStatusHeaderAndLocation.cgi", "GET", null, {}, 302, "Found");
-	printHeader("Cgi Printing Every Variables");
-	await runGoodGgiTest("/cgi/cgiPrintingEveryVariables.cgi", "GET", null, {
-		"accept" : "text/*",
-		"accept-language" : "fr",
-		"user-agent" : "jeSaisPasCeQueCEst",
-		"cookIE" : "qwuehx",
-		"REFERER" : "truc"
-	}, 200, "OK");
-	printHeader("Cgi Closed While Not Reading All");
-	await runGoodGgiTest("/cgi/put/cgiCloseWhileNotReadingAll.cgiput", "PUT", "un dummy body", {
-		"content-length" : "13"
-	}, 200, "OK");
-	printHeader("Cgi Wrong Content Length 1");
-	await runGoodGgiTest("/cgi/cgiWrongContentLength1.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi Wrong Content Length 2");
-	await runGoodGgiTest("/cgi/cgiWrongContentLength2.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi Wrong Content Length 3");
-	await runGoodGgiTest("/cgi/cgiWrongContentLength3.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi Wrong Status 1");
-	await runGoodGgiTest("/cgi/cgiWrongStatus1.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi Wrong Status 2");
-	await runGoodGgiTest("/cgi/cgiWrongStatus2.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi Wrong Status 3");
-	await runGoodGgiTest("/cgi/cgiWrongStatus3.cgi", "GET", null, {}, 502, "Bad Gateway");
-	printHeader("Cgi Status Without Text");
-	await runGoodGgiTest("/cgi/cgiStatusWithoutText.cgi", "GET", null, {}, 200, "OK");
+	const	defaultHeaders = [ "Host: host:port", "Connection: close", "Transfer-Encoding: chunked" ];
+
+	// printHeader("Classic Case");
+	// await runGoodGgiTest("/cgi/cgiTestClassic.cgi", "GET", null, {}, 200, "OK");
+	// printHeader("Chunked Body Output");
+	// await runGoodGgiTest("/cgi/cgiBodyChunked.cgi", "GET", null, {}, 200, "OK");
+	// printHeader("Cgi Body Too Short");
+	// await runGoodGgiTest("/cgi/cgiBodyTooShort.cgi", "GET", null, {}, 200, "OK");
+	// printHeader("Cgi Error In Headers");
+	// await runGoodGgiTest("/cgi/cgiErrorInHeaders.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi No Content Length");
+	// await runGoodGgiTest("/cgi/cgiNoContentLength.cgi", "GET", null, {}, 200, "OK");
+	// printHeader("Cgi No Content Type");
+	// await runGoodGgiTest("/cgi/cgiNoContentType.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi No Response");
+	// await runGoodGgiTest("/cgi/cgiNoResponse.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi No Status Header");
+	// await runGoodGgiTest("/cgi/cgiNoStatusHeader.cgi", "GET", null, {}, 200, "OK");
+	// printHeader("Cgi No Status Header But Location");
+	// await runGoodGgiTest("/cgi/cgiNoStatusHeaderAndLocation.cgi", "GET", null, {}, 302, "Found");
+	// printHeader("Cgi Printing Every Variables");
+	// await runGoodGgiTest("/cgi/cgiPrintingEveryVariables.cgi", "GET", null, {
+	// 	"accept" : "text/*",
+	// 	"accept-language" : "fr",
+	// 	"user-agent" : "jeSaisPasCeQueCEst",
+	// 	"cookIE" : "qwuehx",
+	// 	"REFERER" : "truc"
+	// }, 200, "OK");
+	// printHeader("Cgi Closed While Not Reading All");
+	// await runGoodGgiTest("/cgi/put/cgiCloseWhileNotReadingAll.cgiput", "PUT", "un dummy body", {
+	// 	"content-length" : "13"
+	// }, 200, "OK");
+	// printHeader("Cgi Wrong Content Length 1");
+	// await runGoodGgiTest("/cgi/cgiWrongContentLength1.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi Wrong Content Length 2");
+	// await runGoodGgiTest("/cgi/cgiWrongContentLength2.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi Wrong Content Length 3");
+	// await runGoodGgiTest("/cgi/cgiWrongContentLength3.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi Wrong Status 1");
+	// await runGoodGgiTest("/cgi/cgiWrongStatus1.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi Wrong Status 2");
+	// await runGoodGgiTest("/cgi/cgiWrongStatus2.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi Wrong Status 3");
+	// await runGoodGgiTest("/cgi/cgiWrongStatus3.cgi", "GET", null, {}, 502, "Bad Gateway");
+	// printHeader("Cgi Status Without Text");
+	// await runGoodGgiTest("/cgi/cgiStatusWithoutText.cgi", "GET", null, {}, 200, "OK");
+	printHeader("Cgi Check Body");
+	await runGoodGgiTest("/cgi/put/cgiCheckBody.cgiput", "PUT", "je suis le body", {}, 200, "OK");
+	printHeader("Cgi Check Chunked Body");
+	await sendCgiChunkedRequest("/cgi/put/cgiCheckBody.cgiput", defaultHeaders, [ "je ", "suis ", "le ", "body"], []);
+	printHeader("Cgi Check Chunked Body With Trailers");
+	await sendCgiChunkedRequest("/cgi/put/cgiCheckBody.cgiput", defaultHeaders, [ "je ", "suis ", "le ", "body"], ["test: truc", "machin: bidule"]);
 }
 
 async function	run()
--- tests/scripts/requestsTests/chunkedTests.mjs
+++ tests/scripts/requestsTests/chunkedTests.mjs
@@ -1,5 +1,5 @@
 import { compareBadRequests, compareBadRequestWithValues } from "./compareRequests.mjs"
-import { verifyServersAreRunning, exec, printHeader, generateString, randomInt, COLOR_GREEN, COLOR_RED, COLOR_RESET } from "./utils.mjs"
+import { verifyServersAreRunning, exec, printHeader, generateString, randomInt, COLOR_GREEN, COLOR_RED, COLOR_RESET, createChunkedRequest } from "./utils.mjs"
 
 const	printOK = false;
 
@@ -13,25 +13,6 @@ function	randomStringArray(minArray, maxArray, minString, maxString)
         result.push(generateString(minString, maxString));
     }
     return (result);
-}
-
-function	createChunkedRequest(target, headers, chunks, trailers)
-{
-	let message = "PUT " + target + " HTTP/1.1\r\n";
-	headers.forEach((header) => {
-		message += header + "\r\n";
-	});
-	message += "\r\n";
-	chunks.forEach((chunk) => {
-		message += chunk.length.toString(16) + "\r\n"
-		message += chunk + "\r\n"
-	});
-	message += "0\r\n";
-	trailers.forEach((trailer) => {
-		message += trailer + "\r\n";
-	});
-	message += "\r\n";
-	return (message);
 }
 
 async function	sendRawBadRequest(header, message)
