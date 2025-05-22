132004ea4e31c2e3ae07ce1a0ddcce63e3c1084f
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-05-22 22:39:02 +0200

    Added the same file in the website folder and the unitTest folder

---
+++ code/unitTest/random.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Test CGI</title>
+	</head>
+	<body>
+		<p><?php echo rand(1, 10); ?></p>
+		<p><?php echo 'rand(1, 10)'; ?></p>
+	</body>
+</html>
