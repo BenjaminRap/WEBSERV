5338f1a64f1622c71bf26aa5b6993d12176f5861
Simon Abitbol <sabitbol@z1r5p2.42lyon.fr>
2025-05-25 22:15:24 +0200

    fixed error

--- code/srcs/utils/processAddHeader.cpp
+++ code/srcs/utils/processAddHeader.cpp
@@ -13,11 +13,14 @@ void	processAddHeader(Headers &response_headers, ARequestType &req, const Server
 			response_headers.insert(std::make_pair(it->first, it->second.first));
 	}
 
-	const std::map< std::string, std::pair<std::string, bool> > &routeAddHeader = req.getRoute()->getAddHeader();
-
-	for (std::map< std::string, std::pair<std::string, bool> >::const_iterator it = routeAddHeader.begin(); it != routeAddHeader.end(); ++it)
+	if (req.getRoute() != NULL)
 	{
-		if (it->second.second == true || code < 400)
-			response_headers.insert(std::make_pair(it->first, it->second.first));
+		const std::map< std::string, std::pair<std::string, bool> > &routeAddHeader = req.getRoute()->getAddHeader();
+
+		for (std::map< std::string, std::pair<std::string, bool> >::const_iterator it = routeAddHeader.begin(); it != routeAddHeader.end(); ++it)
+		{
+			if (it->second.second == true || code < 400)
+				response_headers.insert(std::make_pair(it->first, it->second.first));
+		}
 	}
 }
