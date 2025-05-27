e2ed3c132db058345788f966ea0adf008b210193
brappo <brappo@PCTOPACHAT.>
2025-05-27 12:00:50 +0200

    the checkPathINfo tests now also check the query script and the script name

--- tests/scripts/requestsTests/cgi/cgiCheckPathInfo1.cpp
+++ tests/scripts/requestsTests/cgi/cgiCheckPathInfo1.cpp
@@ -4,7 +4,15 @@
 
 # ifndef EXPECTED_PATH_INFO
 #  define EXPECTED_PATH_INFO "/test"
-# endif
+# endif //!EXPECTED_PATH_INFO
+
+# ifndef EXPECTED_SCRIPT_NAME
+#  define EXPECTED_SCRIPT_NAME "/cgi/cgiCheckPathInfo3.cgi"
+# endif //!EXPECTED_SCRIPT_NAME
+
+# ifndef EXPECTED_QUERY_STRING
+#  define EXPECTED_QUERY_STRING "/cgi/cgiCheckPathInfo3.cgi"
+# endif //!EXPECTED_QUERY_STRING
 
 void	printHTTPVar(const char* name)
 {
@@ -14,18 +22,45 @@ void	printHTTPVar(const char* name)
 int main(void)
 {
 	char*	pathInfo = getenv("PATH_INFO");
+	char*	scriptName = getenv("SCRIPT_NAME");
+	char*	queryString = getenv("QUERY_STRING");
 
     printf("Content-Type: text/plain\r\n");
 	if (pathInfo == NULL || std::string(pathInfo) != EXPECTED_PATH_INFO)
 	{
 		printf("StatUS: 500 INTERNAL SERVER ERROR\r\n\r\n");
-		printf("Mauvais path_info, expected : '%s'", EXPECTED_PATH_INFO);
+		printf("Invalid path_info, we got : %s, expected : '%s'", pathInfo, EXPECTED_PATH_INFO);
+		return (0);
 	}
-	else
+	if (scriptName == NULL || std::string(scriptName) != EXPECTED_SCRIPT_NAME)
 	{
-		printf("StatUS: 200 OK\r\n\r\n");
-		printf("path_info OK");
+		printf("StatUS: 500 INTERNAL SERVER ERROR\r\n\r\n");
+		printf("Invalid script name, we got : %s, expected : '%s'", scriptName, EXPECTED_SCRIPT_NAME);
+		return (0);
 	}
+	if (queryString != NULL)
+	{
+		if (*EXPECTED_QUERY_STRING == '\0')
+		{
+			printf("StatUS: 500 INTERNAL SERVER ERROR\r\n\r\n");
+			printf("Query string should be NULL, we got : %s", queryString);
+			return (0);
+		}
+	  	if (std::string(queryString) != EXPECTED_QUERY_STRING)
+		{
+			printf("StatUS: 500 INTERNAL SERVER ERROR\r\n\r\n");
+			printf("Invalid Query string, we got %s, expected : %s", queryString, EXPECTED_QUERY_STRING);
+			return (0);
+		}
+	}
+	else if (*EXPECTED_QUERY_STRING != '\0')
+	{
+			printf("StatUS: 500 INTERNAL SERVER ERROR\r\n\r\n");
+			printf("Query string is NULL, expected : '%s'", EXPECTED_QUERY_STRING);
+			return (0);
+	}
+	printf("StatUS: 200 OK\r\n\r\n");
+	printf("path_info OK");
 
     return 0;
 }
